a8f49fd5ee4e5483e525d78737179fbd676d0164
Andrew Hedges <andrew@hedges.name>
2019-12-14 14:02:20 -0800

    Update Firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,18 +3,18 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-// Initalize Firebase.
-// These details will need to be replaced with the project specific env vars at the start of each new cohort.
-var firebaseConfig = {
-  apiKey: { TCL_X_API_KEY },
-  authDomain: { TCL_X_AUTH_DOMAIN },
-  databaseURL: { TCL_X_DATABASE_URL },
-  projectId: { TCL_X_PROJECT_ID },
-  storageBucket: { TCL_X_STORAGE_BUCKET },
-  messagingSenderId: { TCL_X_SENDER_ID },
-  appId: { TCL_X_APP_ID },
+// Config values for TCL-3
+const config = {
+    apiKey: "AIzaSyAL1ymvsgrfyGGcLBWQYYXqdixr0tPzuS8",
+    authDomain: "tcl-3-smart-shopping-list.firebaseapp.com",
+    databaseURL: "https://tcl-3-smart-shopping-list.firebaseio.com",
+    projectId: "tcl-3-smart-shopping-list",
+    storageBucket: "tcl-3-smart-shopping-list.appspot.com",
+    messagingSenderId: "95252731516",
+    appId: "1:95252731516:web:7e2b9f93b34ab1f8c25bf9"
 };
 
-let fb = firebase.initializeApp(config);
+// Initalize Firebase.
+const fb = firebase.initializeApp(config);
 
 export { fb };
