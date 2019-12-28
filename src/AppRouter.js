945e2808014ef54747e8b305f2b936f34a744d4c
Andrew Hedges <andrew@hedges.name>
2019-12-28 10:21:54 -0800

    Fix link navigation by moving BrowserRouter up the tree

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Route, Switch } from 'react-router';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AddItem from './pages/AddItem';
 import List from './pages/List';
 import HomePage from './pages/HomePage';
 
 export default function AppRouter() {
   return (
-    <>
+    <BrowserRouter>
       <Switch>
         <Route exact path="/" component={List} />
         <Route path="/add-item" component={AddItem} />
         <Route path="/home" component={HomePage} />
       </Switch>
-    </>
+    </BrowserRouter>
   );
 }
