914dd2e8fbd11fe87b572fbbc68e175339baa450
Amber <ajiles91@gmail.com>
2019-12-28 15:05:42 -0500

    added tabs to bottom of home screen

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,19 +4,19 @@ import './HomePage.css';
 
 export default function HomePage() {
   return (
-    <>
+    <div className="whole-page">
       <h1>here's our homepage!</h1>
       <div className="container-container">
         <div className="container">
-          <NavLink activeClassName="left-tab" to="/add-item">
+          <NavLink className="left-tab" to="/add-item">
             to add item
           </NavLink>
 
-          <NavLink activeClassName="right-tab" to="/">
+          <NavLink className="right-tab" to="/">
             to list
           </NavLink>
         </div>
       </div>
-    </>
+    </div>
   );
 }
