b246848f6519e21ff37e6042e4962b1ef690f2a5
Steve Gardner <steve.gardner@zapier.com>
2020-01-02 22:45:16 +1100

    Create a firebase doc using the item name as the doc ID
    - This will allow easier debugging. In a more complex project, we'd want to use a more complex ID.
    - Implement useState for the radio button handler
    - Wrap radio inputs with their respective label elements for better accessibility/UX

--- src/pages/AddItem.js
+++ src/pages/AddItem.js
@@ -1,17 +1,19 @@
-import React, { useState, useInput } from 'react';
+import React, { useState } from 'react';
 import { withFirestore } from 'react-firestore';
 import AddItemHeader from './AddItemHeader';
 
+const expectedPurchase = { soon: 7, verySoon: 14, notSoon: 30 };
+
 const AddItem = ({ firestore }) => {
   const [name, setName] = useState('');
-
-  const [data] = useState({ soon: 7, very_soon: 14, not_soon: 30 });
-
-  const [nextExpectedPurchase, setNextExpectedPurchase] = useInput('');
+  // Try to keep your types consistent. If the setter always sets a number,
+  // then initialize with a number
+  const [nextExpectedPurchase, setNextExpectedPurchase] = useState(0);
 
   // Send the new item to Firebase
-  const addItem = name => {
-    firestore.collection('items').add({ name });
+  const addItem = () => {
+    const doc = firestore.collection('items').doc(name);
+    doc.set({ name, nextExpectedPurchase });
   };
 
   // The state every time an event happens
@@ -19,10 +21,15 @@ const AddItem = ({ firestore }) => {
     setName(event.target.value);
   };
 
+  // Value returns as a string. We need to be sure to send a number.
+  const handleSelect = event => {
+    setNextExpectedPurchase(parseInt(event.target.value, 10));
+  };
+
   // Handle the click of the Add Item button on the form
   const handleSubmit = event => {
     event.preventDefault();
-    addItem(name);
+    addItem();
     setName('');
   };
 
@@ -42,34 +49,44 @@ const AddItem = ({ firestore }) => {
         </label>
         <input type="submit" value="Add Item" />
         <div>
-          <input
-            type="radio"
-            id={data.soon}
-            value={data.soon}
-            checked={data.soon === nextExpectedPurchase}
-            onChange={setNextExpectedPurchase}
-          />
-          <label>Soon</label>
+          {/* Good habit to wrap inputs with the label.
+              This way, clicking the label also selects the input. 
+              If you wanted, this could be its own component.
+              Or we could use <select> instead of radio buttons. */}
+          <label>
+            <input
+              type="radio"
+              id={expectedPurchase.soon}
+              value={expectedPurchase.soon}
+              checked={expectedPurchase.soon === nextExpectedPurchase}
+              onChange={handleSelect}
+            />
+            Soon
+          </label>
         </div>
         <div>
-          <input
-            type="radio"
-            id={data.very_soon}
-            value={data.very_soon}
-            checked={data.very_soon === nextExpectedPurchase}
-            onChange={setNextExpectedPurchase}
-          />
-          <label>Very Soon</label>
+          <label>
+            <input
+              type="radio"
+              id={expectedPurchase.verySoon}
+              value={expectedPurchase.verySoon}
+              checked={expectedPurchase.verySoon === nextExpectedPurchase}
+              onChange={handleSelect}
+            />
+            Very Soon
+          </label>
         </div>
         <div>
-          <input
-            type="radio"
-            id={data.not_soon}
-            value={data.not_soon}
-            checked={data.not_soon === nextExpectedPurchase}
-            onChange={setNextExpectedPurchase}
-          />
-          <label>Not Soon</label>
+          <label>
+            <input
+              type="radio"
+              id={expectedPurchase.notSoon}
+              value={expectedPurchase.notSoon}
+              checked={expectedPurchase.notSoon === nextExpectedPurchase}
+              onChange={handleSelect}
+            />
+            Not Soon
+          </label>
         </div>
       </form>
     </>
