72ccdadb9cc31e40a69cc8ce29940c8aebb5db65
Amber <ajiles91@gmail.com>
2020-01-10 02:56:16 -0500

    added 2nd firestore collection to list.js and and working on storing user input for token to join list

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './HomePage.css';
 import NavTabs from '../components/NavTabs';
 
 export default function HomePage() {
+  const [userJoinToken, setUserJoinToken] = useState('');
+
+  const getJoinToken = () => window.localStorage.getItem('joinToken');
+  const [joinToken] = useState(getJoinToken);
+  // const [joinToken] = useState('');
+
+  // useEffect(() =>  {
+  //   window.localStorage.setItem('joinToken', userJoinToken);
+  //   },[userJoinToken]
+  // );
+  console.log('the join token below useEffect:', joinToken);
+  const handleChange = event => {
+    setUserJoinToken(event.target.value);
+  };
+
+  // token to use:  low stone iambic
+  const handleSubmit = event => {
+    event.preventDefault();
+    window.localStorage.setItem('joinToken', userJoinToken);
+    console.log('the join token inside of console.log:', userJoinToken);
+    setUserJoinToken('');
+  };
+
   return (
     <div className="whole-page">
       <h1>Here's our homepage! Go to your list or add an item below.</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Need to join a list?</label>
+
+        <input
+          value={userJoinToken}
+          placeholder="Add token here"
+          type="text"
+          onChange={handleChange}
+        />
+
+        <input type="submit" value="Grab Your List" />
+      </form>
       <NavTabs />
     </div>
   );
--- src/pages/List.js
+++ src/pages/List.js
@@ -4,9 +4,33 @@ import Loading from '../components/Loading';
 import NavTabs from '../components/NavTabs';
 
 const List = () => {
+  const getJoinToken = () => window.localStorage.getItem('joinToken');
   const getStoredToken = () => window.localStorage.getItem('token');
+
   return (
     <>
+      <FirestoreCollection
+        // Specify the path to the collection you're pulling data from
+        path="items"
+        // Sort the data
+        sort="name"
+        // Only fetch the items associated with the token saved in localStorage
+        filter={['token', '==', getJoinToken()]}
+        // isLoading = is a Boolean that represents the loading status for the firebase query. true until an initial payload from Firestore is received.
+        // data = an Array containing all of the documents in the collection. Each item will contain an id along with the other data contained in the document.
+        render={({ isLoading, data }) => {
+          return isLoading ? (
+            <Loading />
+          ) : (
+            <ul>
+              {data.map(item => (
+                <li key={item.id}>{item.name}</li>
+              ))}
+            </ul>
+          );
+        }}
+      />
+
       <FirestoreCollection
         // Specify the path to the collection you're pulling data from
         path="items"
@@ -28,6 +52,7 @@ const List = () => {
           );
         }}
       />
+
       <NavTabs />
     </>
   );
