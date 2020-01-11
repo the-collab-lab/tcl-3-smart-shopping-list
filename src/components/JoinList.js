b918f383243eec343b652565a388bd01f19793ec
Amber <ajiles91@gmail.com>
2020-01-11 14:06:12 -0500

    fixed form for joining list

--- src/components/JoinList.js
+++ src/components/JoinList.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../pages/HomePage.css';
 
 const JoinList = () => {
-  return <div>hello there testing</div>;
+  const [userJoinToken, setUserJoinToken] = useState('');
+
+  const getJoinToken = () => window.localStorage.getItem('token');
+  const [joinToken] = useState(getJoinToken);
+
+  useEffect(() => {
+    window.localStorage.setItem('token', joinToken);
+  }, [joinToken]);
+  console.log('the join token added to localStorage:', joinToken);
+  const handleChange = event => {
+    setUserJoinToken(event.target.value);
+  };
+
+  // token to use:  low stone iambic, "kelp bruce puny"
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    window.localStorage.setItem('token', userJoinToken);
+    console.log('the join token inside of console.log:', userJoinToken);
+    setUserJoinToken('');
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Need to join a list?</label>
+
+        <input
+          value={userJoinToken}
+          placeholder="Add token here"
+          type="text"
+          onChange={handleChange}
+        />
+
+        <input type="submit" value="Grab Your List" />
+      </form>
+    </div>
+  );
 };
 
 export default JoinList;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,43 +4,11 @@ import NavTabs from '../components/NavTabs';
 import JoinList from '../components/JoinList';
 
 export default function HomePage() {
-  const [userJoinToken, setUserJoinToken] = useState('');
-
-  const getJoinToken = () => window.localStorage.getItem('token');
-  const [joinToken] = useState(getJoinToken);
-
-  useEffect(() => {
-    window.localStorage.setItem('token', joinToken);
-  }, [joinToken]);
-  console.log('the join token added to localStorage:', joinToken);
-  const handleChange = event => {
-    setUserJoinToken(event.target.value);
-  };
-
-  // token to use:  low stone iambic
-  const handleSubmit = event => {
-    event.preventDefault();
-    window.localStorage.setItem('token', userJoinToken);
-    console.log('the join token inside of console.log:', userJoinToken);
-    setUserJoinToken('');
-  };
-
   return (
     <div className="whole-page">
       <h1>Here's our homepage! Go to your list or add an item below.</h1>
       <JoinList />
-      <form onSubmit={handleSubmit}>
-        <label>Need to join a list?</label>
 
-        <input
-          value={userJoinToken}
-          placeholder="Add token here"
-          type="text"
-          onChange={handleChange}
-        />
-
-        <input type="submit" value="Grab Your List" />
-      </form>
       <NavTabs />
     </div>
   );
