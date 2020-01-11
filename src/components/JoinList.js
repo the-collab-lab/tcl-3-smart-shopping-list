950f8e71764660371788908004226237b0bce7c1
MonicaDJohnson <39500978+MonicaDJohnson@users.noreply.github.com>
2020-01-11 13:36:41 -0500

    testing, added new component

---
+++ src/components/JoinList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../pages/HomePage.css';
+
+const JoinList = () => {
+  return <div>hello there testing</div>;
+};
+
+export default JoinList;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './HomePage.css';
 import NavTabs from '../components/NavTabs';
+import JoinList from '../components/JoinList';
 
 export default function HomePage() {
   return (
     <div className="whole-page">
       <h1>Here's our homepage! Go to your list or add an item below.</h1>
+      <JoinList />
       <NavTabs />
     </div>
   );
