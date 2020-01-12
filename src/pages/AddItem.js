f82f69113fa1d17923c50d93d32c97e9551a8004
Nikema Prophet <nikema.prophet@popschools.com>
2020-01-12 04:21:08 -0800

    Checking for duplicates and alerting is working

--- src/pages/AddItem.js
+++ src/pages/AddItem.js
@@ -8,6 +8,97 @@ import { ListContext } from '../listContext';
 
 const expectedPurchase = { soon: 7, kindOfSoon: 14, notSoon: 30 };
 
-const AddItem = ({ firestore }) => {};
+const AddItem = ({ firestore }) => {
+  const { token, setShoppingList, shoppingList } = useContext(ListContext);
+
+  const [name, setName] = useState('');
+  const [nextExpectedPurchase, setNextExpectedPurchase] = useState(0);
+  const [duplicate, setDuplicate] = useState(false);
+
+  const addItem = name => {
+    firestore.collection('items').add({ name, token, nextExpectedPurchase });
+  };
+
+  // The state every time an event happens
+  const handleChange = event => {
+    setName(event.target.value);
+  };
+
+  const handleSelect = event => {
+    setNextExpectedPurchase(parseInt(event.target.value, 10));
+  };
+  const checkForDuplicates = name => {
+    let normalizedName = normalizeName(name);
+
+    let normalizedList = shoppingList.map(item => normalizeName(item.name));
+    setDuplicate(normalizedList.includes(normalizedName));
+
+    console.log('normalizedList ', normalizedList);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    checkForDuplicates();
+    addItem(name, token, nextExpectedPurchase);
+    setName('');
+  };
+
+  return (
+    <>
+      <AddItemHeader />
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Add Item:
+          <input
+            value={name}
+            placeholder="apples"
+            type="text"
+            onChange={handleChange}
+          />
+        </label>
+        <input type="submit" value="Add Item" />
+        <div>
+          <label>
+            <input
+              type="radio"
+              id={expectedPurchase.soon}
+              value={expectedPurchase.soon}
+              checked={expectedPurchase.soon === nextExpectedPurchase}
+              onChange={handleSelect}
+            />
+            Soon
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="radio"
+              id={expectedPurchase.verySoon}
+              value={expectedPurchase.kindOfSoon}
+              checked={expectedPurchase.kindOfSoon === nextExpectedPurchase}
+              onChange={handleSelect}
+            />
+            Kind of Soon
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="radio"
+              id={expectedPurchase.notSoon}
+              value={expectedPurchase.notSoon}
+              checked={expectedPurchase.notSoon === nextExpectedPurchase}
+              onChange={handleSelect}
+            />
+            Not Soon
+          </label>
+        </div>
+      </form>
+
+      {duplicate ? <ItemError /> : null}
+    </>
+  );
+};
 // Wrap this component in the higher order component withFirestore to directly access the database
 export default withFirestore(AddItem);
