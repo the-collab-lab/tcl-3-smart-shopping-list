bfaa04eda9ed81c22b4c444d9327601323afdd41
MonicaDJohnson <39500978+MonicaDJohnson@users.noreply.github.com>
2020-01-16 21:09:31 -0500

    line 6 hiddenbutton broken

---
+++ src/components/HiddenButton.js
@@ -0,0 +1,11 @@
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import '../pages/HomePage.css';
+
+// // const [hiddenButton, setHiddenButton] = useState (false);
+// // <div>
+// // <button onClick={() => setHiddenButton(true)}>Click here to use an existing token</button>
+// // {/* {hiddenButton && 'token'} */}
+// // </div>
+
+// export default hiddenButton;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './HomePage.css';
 import NavTabs from '../components/NavTabs';
 import JoinList from '../components/JoinList';
+import HiddenButton from '../components/HiddenButton';
 
 export default function HomePage() {
   return (
     <div className="whole-page">
       <h1>Here's our homepage! Go to your list or add an item below.</h1>
-     
+
       <JoinList />
       <NavTabs />
+      <HiddenButton />
     </div>
   );
 }
