a59cfdc5c1faadf556976ed6bb727bee05c902ae
MonicaDJohnson <39500978+MonicaDJohnson@users.noreply.github.com>
2020-01-18 22:43:05 -0500

    commented out code, recieving error messages

--- src/components/HiddenButton.js
+++ src/components/HiddenButton.js
@@ -2,10 +2,30 @@
 // import { Link } from 'react-router-dom';
 // import '../pages/HomePage.css';
 
-// // const [hiddenButton, setHiddenButton] = useState (false);
-// // <div>
-// // <button onClick={() => setHiddenButton(true)}>Click here to use an existing token</button>
-// // {/* {hiddenButton && 'token'} */}
-// // </div>
-
+// const hiddenButton = () => {
+//   const [hiddenThing, setHiddenThing] = useState(true);
+//   return (
+//     <div>
+//     </div>
+//   )
+// };
 // export default hiddenButton;
+
+//I commented out this code at the top because I would not be able to push any of it to my branch if I didn't.
+
+//All of the code in this bottom section is commented out, also. These are the last changes that I made prior to using Stacie's suggestion above.
+
+// //  const hideButton
+// // const setHideButton
+// const hiddenButtons = () => {
+//   const [hideButton, setHideButton] = useState ("");  return
+//   (<div>
+//     <button onClick={() => setHideButton(true)}>Click here to use an existing token</button>
+//       {/* {hiddenButton && 'token'}  */}
+//     </div>)// const hiddenButtons = () => {[hideButton, setHideButton] = useState (false)
+// //       return (<div>
+// //         <button onClick={() => setHideButton(true)}>Click here to use an existing token</button>
+// //         {/* {hiddenButton && 'token'} */}
+// //         </div>)
+//   };
+//   export default hiddenButtons;
