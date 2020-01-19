4ceef7fdb7fb2db40be79c7fd4060044a32b0920
Amber <ajiles91@gmail.com>
2020-01-19 16:23:38 -0500

    fix: more descriptive error message in ErrorMessage.js

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function ErrorMessage() {
   return (
     <div className="whole-page">
-      <h1>Token Not Found</h1>
+      <h1>
+        List not found! Try going back to the homepage to join another list.
+      </h1>
     </div>
   );
 }
