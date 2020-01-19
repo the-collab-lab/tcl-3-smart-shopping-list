9986713fd8f413aad9809ec27769acaa3bd94381
Nikema Prophet <nikema.prophet@popschools.com>
2020-01-19 10:59:51 -0800

    Refactor: Change normalizeName function to use regex

--- src/lib/normalizeName.js
+++ src/lib/normalizeName.js
@@ -1,13 +1,9 @@
 //normalized function
 const normalizeName = name => {
-  name = name.toLowerCase().trim();
-  let normalizedName = '';
-  let symbol = `.,;:!?"`;
-  for (let i = 0; i < name.length; i++) {
-    if (!symbol.includes(name[i])) {
-      normalizedName += name.slice(i, i + 1);
-    }
-  }
-  return normalizedName;
+  return name
+    .replace(/[,.;:!-?]/g, '')
+    .toLowerCase()
+    .replace(/\s+\s/g, ' ')
+    .trim();
 };
 export default normalizeName;
