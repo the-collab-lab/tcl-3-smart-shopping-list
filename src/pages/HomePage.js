ee5ef99e1e192eafa1300c654df667f60d3b7a8b
Nikema Prophet <nikema.prophet@popschools.com>
2020-01-21 16:47:36 -0800

    Move getting and setting token to its own context
    I took the handling the token out of list context and made the changes in the other files so that we always access the token from tokenContext.js or localStorage.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,17 +4,6 @@ import NavTabs from '../components/NavTabs';
 import JoinList from '../components/JoinList';
 import HiddenButton from '../components/HiddenButton';
 
-// export default function HomePage() {
-//   return (
-//     <div className="whole-page">
-//       <h1>Here's our homepage! Go to your list or add an item below.</h1>
-//       <JoinList />
-//       <NavTabs />
-//       <HiddenButton />
-//     </div>
-//   );
-// }
-
 class HomePage extends Component {
   constructor(props) {
     super(props);
