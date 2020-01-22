dd9ff301c08dd4163e6f15b64ce0d5e800283c13
Nikema Prophet <nikema.prophet@popschools.com>
2020-01-21 22:55:24 -0800

    Create displayList function in listContext.js

--- src/listContext.js
+++ src/listContext.js
@@ -1,12 +1,20 @@
 import React, { useState, useContext } from 'react';
 import normalizeName from './lib/normalizeName';
-import { withFirestore } from 'react-firestore';
+import { withFirestore, FirestoreCollection } from 'react-firestore';
 import { TokenContext } from './tokenContext';
+import Loading from './components/Loading';
+import ErrorMessage from './components/ErrorMessage';
 
 const ListContext = React.createContext();
 
 const ListContextProvider = props => {
-  const { token } = useContext(TokenContext);
+  const {
+    token,
+    setToken,
+    initialToken,
+    getLocalStorageToken,
+    setLocalStorageToken,
+  } = useContext(TokenContext);
   const { firestore } = props;
   const itemsRef = firestore.collection('items');
 
@@ -46,10 +54,49 @@ const ListContextProvider = props => {
     }
   }
 
+  function displayList(token) {
+    return (
+      <FirestoreCollection
+        // Specify the path to the collection you're pulling data from
+        path="items"
+        // Sort the data
+        sort="name"
+        // Only fetch the items associated with the token saved in localStorage
+        filter={['token', '==', token || getLocalStorageToken()]}
+        // isLoading = is a Boolean that represents the loading status for the firebase query. true until an initial payload from Firestore is received.
+        // data = an Array containing all of the documents in the collection. Each item will contain an id along with the other data contained in the document.
+        render={({ isLoading, data }) => {
+          if (!isLoading && data.length === 0) {
+            // If a list wasn't found create a new token in case the user adds an item. That way a new list will be created
+
+            // Not working. TODO: Maybe make a function that clears the token in storage and generates a new one?
+
+            setLocalStorageToken(initialToken());
+            setToken(getLocalStorageToken());
+            return <ErrorMessage />;
+          }
+
+          return isLoading ? (
+            // TODO: Make a display list function is listContext.js
+            <Loading />
+          ) : (
+            <ul>
+              {setShoppingList(data)}
+              {shoppingList.map((item, index) => (
+                <li key={index}>{item.name}</li>
+              ))}
+            </ul>
+          );
+        }}
+      />
+    );
+  }
+
   return (
     <ListContext.Provider
       value={{
         token,
+        displayList,
         shoppingList,
         setShoppingList,
         fetchList,
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,57 +1,17 @@
 import React, { useContext } from 'react';
-import { FirestoreCollection } from 'react-firestore';
-import Loading from '../components/Loading';
 import NavTabs from '../components/NavTabs';
-import ErrorMessage from '../components/ErrorMessage';
 import HomePageButton from '../components/HomePageButton';
 import { ListContext } from '../listContext';
 import { TokenContext } from '../tokenContext';
 
 const List = () => {
-  const {
-    token,
-    setToken,
-    initialToken,
-    getLocalStorageToken,
-    setLocalStorageToken,
-  } = useContext(TokenContext);
-  const { setShoppingList, shoppingList } = useContext(ListContext);
+  const { token } = useContext(TokenContext);
+  const { displayList } = useContext(ListContext);
 
   return (
     <>
       <HomePageButton />
-
-      <FirestoreCollection
-        // Specify the path to the collection you're pulling data from
-        path="items"
-        // Sort the data
-        sort="name"
-        // Only fetch the items associated with the token saved in localStorage
-        filter={['token', '==', token || getLocalStorageToken()]}
-        // isLoading = is a Boolean that represents the loading status for the firebase query. true until an initial payload from Firestore is received.
-        // data = an Array containing all of the documents in the collection. Each item will contain an id along with the other data contained in the document.
-        render={({ isLoading, data }) => {
-          if (!isLoading && data.length === 0) {
-            // If a list wasn't found create a new token in case the user adds an item. That way a new list will be created
-
-            // Not working. TODO: Maybe make a function that clears the token in storage and generates a new one?
-            setLocalStorageToken(initialToken());
-            setToken(getLocalStorageToken());
-            return <ErrorMessage />;
-          }
-
-          return isLoading ? (
-            <Loading />
-          ) : (
-            <ul>
-              {setShoppingList(data)}
-              {shoppingList.map((item, index) => (
-                <li key={index}>{item.name}</li>
-              ))}
-            </ul>
-          );
-        }}
-      />
+      {displayList(token)}
       <NavTabs />
     </>
   );
