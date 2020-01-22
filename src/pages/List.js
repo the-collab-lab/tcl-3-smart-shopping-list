7061df0275aa423104332a8ab716f5ecd9ea9bb8
Nikema Prophet <nikema.prophet@popschools.com>
2020-01-21 17:54:57 -0800

    Make sure token is getting fetched and saved to localstorage using the TokeContext functions

--- src/pages/List.js
+++ src/pages/List.js
@@ -8,7 +8,9 @@ import { ListContext } from '../listContext';
 import { TokenContext } from '../tokenContext';
 
 const List = () => {
-  const { token } = useContext(TokenContext);
+  const { token, getLocalStorageToken, setLocalStorageToken } = useContext(
+    TokenContext,
+  );
   const { setShoppingList, shoppingList } = useContext(ListContext);
 
   return (
@@ -21,12 +23,12 @@ const List = () => {
         // Sort the data
         sort="name"
         // Only fetch the items associated with the token saved in localStorage
-        filter={['token', '==', token]}
+        filter={['token', '==', token || getLocalStorageToken]}
         // isLoading = is a Boolean that represents the loading status for the firebase query. true until an initial payload from Firestore is received.
         // data = an Array containing all of the documents in the collection. Each item will contain an id along with the other data contained in the document.
         render={({ isLoading, data }) => {
           if (!isLoading && data.length === 0) {
-            localStorage.setItem('token', token);
+            setLocalStorageToken(token);
             return <ErrorMessage />;
           }
 
