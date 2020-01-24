50c44138978205ff24783b991a8dfa5294abad61
Nikema Prophet <nikema.prophet@popschools.com>
2020-01-23 17:44:22 -0800

    Fix critical bug on HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 
 import useListToken from '../useListToken';
 import { ListContext } from '../listContext';
@@ -6,15 +6,8 @@ import List from './List';
 import Welcome from '../components/Welcome';
 
 const HomePage = () => {
-  const { saveToken, token } = useListToken();
-  const { fetchList, validToken } = useContext(ListContext);
-
-  useEffect(() => {
-    fetchList(token);
-    if (validToken(token)) {
-      saveToken(token);
-    }
-  }, [validToken, token, fetchList, saveToken]);
+  const { token } = useListToken();
+  const { validToken } = useContext(ListContext);
 
   return <>{validToken(token) ? <List /> : <Welcome />}</>;
 };
