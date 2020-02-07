6faaeeab883ff817fe4e32ae1a6cd8e1a587d5e4
Nikema Prophet <nikema.prophet@popschools.com>
2020-02-07 01:09:16 -0800

    Simplify welcome prompt
    Instead of making the welcome prompt two parts it's not just one view. The join list field and button are responsive.

--- src/components/JoinList.js
+++ src/components/JoinList.js
@@ -2,23 +2,33 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import '../pages/HomePage.css';
 import useListToken from '../useListToken';
+import AppHeader from './AppHeader';
+import { Link } from 'react-router-dom';
+import {
+  Input,
+  Button,
+  Segment,
+  Divider,
+  Container,
+  Responsive,
+} from 'semantic-ui-react';
 
 const JoinList = () => {
   const { saveToken } = useListToken();
   const history = useHistory();
 
   const [joinToken, setJoinToken] = useState('');
-  const [loading, setLoading] = useState();
+  // const [loading, setLoading] = useState();
 
   const handleChange = event => {
     setJoinToken(event.target.value);
   };
 
   const handleSubmit = event => {
-    setLoading(true);
+    // setLoading(true);
     event.preventDefault();
     saveToken(joinToken);
-    joinToken && setLoading(false);
+    // joinToken && setLoading(false);
 
     return joinToken && history.push('/list');
   };
@@ -26,27 +36,59 @@ const JoinList = () => {
   // token to use: jimmy torn jolt, conner oaken liz
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} className="form-flex-container">
-        <label htmlFor="join-list-input">
-          Need to join a list? Enter your share code below, then tap "Grab Your
-          List"
-        </label>
+    <Container textAlign="center">
+      <AppHeader>Welcome to Your Smart Shopping List!</AppHeader>
+      <Segment basic textAlign="center">
+        <form onSubmit={handleSubmit} className="form-flex-container">
+          <p>
+            Need to join a list? Enter your share code below, then tap "Grab
+            Your List"
+          </p>
 
-        <input
-          className="token-input"
-          name="join-list-input"
-          placeholder="Enter share code here"
-          type="text"
-          onChange={handleChange}
-        />
+          <Responsive
+            as={Input}
+            minWidth={432}
+            action={{ color: 'blue', content: 'Grab Your List' }}
+            size="large"
+            icon="list"
+            iconPosition="left"
+            placeholder="Enter share code"
+            onChange={handleChange}
+          />
+          <Responsive maxWidth={431}>
+            <Input
+              size="large"
+              icon="list"
+              iconPosition="left"
+              placeholder="Enter share code"
+              onChange={handleChange}
+            />
+            <Button
+              style={{ margin: '1em' }}
+              className="grab-list-button"
+              size="large"
+              color="blue"
+              type="submit"
+            >
+              Grab Your List
+            </Button>
+          </Responsive>
 
-        <button type="submit" className="cta-button">
-          Grab Your List
-        </button>
-        {loading && <p>Fetching your list...</p>}
-      </form>
-    </div>
+          <Divider horizontal>Or</Divider>
+
+          <Button
+            as={Link}
+            to="/add-item"
+            size="large"
+            color="teal"
+            content="Create a New Shopping List"
+            icon="add"
+            labelPosition="left"
+          />
+        </form>
+      </Segment>
+      {/* {loading && <p>Fetching your list...</p>} */}
+    </Container>
   );
 };
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,14 @@
 import React, { useContext, useState } from 'react';
 import { getCurrentToken } from '../useListToken';
 import List from './List';
-import Welcome from '../components/Welcome';
 import { ListContext } from '../listContext';
+import JoinList from '../components/JoinList';
 
 const HomePage = props => {
   const { fetchList } = useContext(ListContext);
   const [list] = useState(() => fetchList(getCurrentToken()));
 
-  return <>{list.length > 0 ? <List /> : list && <Welcome />}</>;
+  return <>{list.length > 0 ? <List /> : list && <JoinList />}</>;
 };
 // afar weld impel
 export default HomePage;
